20d85d0a1aa50d21e8f12dbafeb489e23a7c1257
git config --global user.name Your <sonanedrushti.26@gmail.com>
2025-08-21 14:58:21 +0530

    drushti-fastapi

---
+++ Drushti/app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# Create the FastAPI app
+app = FastAPI()
+
+# Define request body using Pydantic model
+class Numbers(BaseModel):
+    num1: float
+    num2: float
+
+# POST endpoint at /add
+@app.post("/add")
+def add_numbers(data: Numbers):
+    return {"received": {"num1": data.num1, "num2": data.num2, "sum":data.num1+data.num2}}
+
